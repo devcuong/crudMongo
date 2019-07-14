6844c69aca84bdfb8a1fd681683d8e43a8a662e4
devcuong <viettxt@gmail.com>
2019-07-14 18:21:47 +0700

    ok

--- controllers/trangChuController.js
+++ controllers/trangChuController.js
@@ -5,11 +5,6 @@ const Truyen = mongoose.model("Truyen");
 const utils = require("../utils/navRender.js");
 
 router.get("/",(req,res)=>{
-	/*res.render("home/homePage",{
-		layout: 'defaultLayout.hbs',
-		//hotList: docs
-		
-	});*/
 	var q = Truyen.find({danh_gia: 1}).limit(2);
 		q.exec(function(err, docs) {
 			if(!err)
