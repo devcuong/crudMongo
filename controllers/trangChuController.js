437a15f23a2e21cdcfb7ec7de08e706e3d4e682d
devcuong <viettxt@gmail.com>
2019-07-24 00:02:03 +0700

    abc

--- controllers/trangChuController.js
+++ controllers/trangChuController.js
@@ -5,91 +5,91 @@ const Truyen = mongoose.model("Truyen");
 require('dotenv').config()
 
 router.get("/", (req, res) => {
-	var q = Truyen.find({ danh_gia: 1 }).limit(13);
-	q.exec(function (err, docs) {
-		if (!err) {
-			res.render("home/homePage", {
-				layout: 'defaultLayout.hbs',
-				list: docs
-			});
-		}
-		else {
-			console.log(err);
-		}
+    var q = Truyen.find({ danh_gia: 1 }).limit(13);
+    q.exec(function(err, docs) {
+        if (!err) {
+            res.render("home/homePage", {
+                layout: 'defaultLayout.hbs',
+                list: docs
+            });
+        } else {
+            console.log(err);
+        }
 
-	});
+    });
 });
 
 router.get("/apiHot/:tl", (req, res) => {
-	var tl = req.params.tl;
-	if (tl != "") {
-		var q = Truyen.find().sort({ _id: -1 }).limit(13);
-		if (tl != "all") {
-			q = Truyen.find({ the_loai: tl }).sort({ _id: -1 }).limit(13);
-		}
-		q.exec(function (err, docs) {
-			if (!err) {
-				res.json(docs);
-			} else {
-				console.log(err);
+    var tl = req.params.tl;
+    if (tl != "") {
+        var q = Truyen.find().sort({ _id: -1 }).limit(13);
+        if (tl != "all") {
+            q = Truyen.find({ the_loai: tl }).sort({ _id: -1 }).limit(13);
+        }
+        q.exec(function(err, docs) {
+            if (!err) {
+                res.json(docs);
+            } else {
+                console.log(err);
 
-			}
+            }
 
-		});
-	}
+        });
+    }
 })
 
 router.get("/apiNew/:tl", (req, res) => {
-	var tl = req.params.tl;
-	if (tl != "") {
-		var q = Truyen.find().sort({ _id: -1 }).limit(16);
-		if (tl != "all") {
-			q = Truyen.find({ the_loai: tl }).limit(16);
-		}
-		q.exec(function (err, docs) {
-			if (!err) {
-				res.json(docs);
-			} else {
-				console.log(err);
+    var tl = req.params.tl;
+    if (tl != "") {
+        var q = Truyen.find().sort({ _id: -1 }).limit(16);
+        if (tl != "all") {
+            q = Truyen.find({ the_loai: tl }).limit(16);
+        }
+        q.exec(function(err, docs) {
+            if (!err) {
+                res.json(docs);
+            } else {
+                console.log(err);
 
-			}
+            }
 
-		});
-	}
+        });
+    }
 })
 
 router.get("/apiFull/all", (req, res) => {
-	var q = Truyen.find({ trang_thai: "Full" }).limit(12);
-	q.exec(function (err, docs) {
-		if (!err) {
-			res.json(docs);
-		} else {
-			console.log(err);
+    var q = Truyen.find({ trang_thai: "Full" }).limit(12);
+    q.exec(function(err, docs) {
+        if (!err) {
+            res.json(docs);
+        } else {
+            console.log(err);
 
-		}
+        }
 
-	});
+    });
 })
 
 router.get("/the-loai/:tl", (req, res) => {
-	var tl = req.params.tl;
-	if (tl != "") {
-		var q = Truyen.find().sort({ _id: -1 }).limit(16);
-		if (tl != "all") {
-			q = Truyen.find({ the_loai: tl }).limit(16);
-		}
-		q.exec(function (err, docs) {
-			if (!err) {
-				res.render("home/catPage", {
-					layout: 'defaultLayout.hbs',
-					hostname: process.env.SERVER_NAME
-				});
-			} else {
-				console.log(err);
-			}
+    var tl = req.params.tl;
+    if (tl != "") {
+        var q = Truyen.find().sort({ _id: -1 }).limit(16);
+        if (tl != "all") {
+            q = Truyen.find({ the_loai: tl }).sort({ _id: -1 }).limit(16);
+        }
+        q.exec(function(err, docs) {
+            if (!err) {
+                res.render("home/catPage", {
+                    layout: 'defaultLayout.hbs',
+                    list: docs,
+                    hostname: process.env.SERVER_NAME
+                });
+            } else {
+                console.log(err);
+            }
 
-		});
-	}
+        });
+    }
 });
 
 
--- models/truyen.model.js
+++ models/truyen.model.js
@@ -1,40 +1,40 @@
 const mongoose = require('mongoose');
 var truyenSchema = new mongoose.Schema({
 
-	
-	ten_truyen:{
-		type: String,
-		required: "this file is required"
-	},
-	url_truyen:{
-		type: String
-	},
-	trang_thai:{
-		type: String
-	},
-	url_hinh:{
-		type: String
-	},
-	so_chuong:{
-		type: String
-	},
-	views:{
-		type: String
-	},
 
-	danh_gia:{
-		type: Number
-	},
+    ten_truyen: {
+        type: String,
+        required: "this file is required"
+    },
+    url_truyen: {
+        type: String
+    },
+    trang_thai: {
+        type: String
+    },
+    url_hinh: {
+        type: String
+    },
+    so_chuong: {
+        type: String
+    },
+    views: {
+        type: String
+    },
 
-	update_time:{
-		type: String
-	}
+    danh_gia: {
+        type: Number
+    },
+
+    update_time: {
+        type: String
+    },
+
+    tac_gia: {
+        type: String
+    }
 
 }, {
-	versionKey: false // You should be aware of the outcome after set to false
-});	
+    versionKey: false // You should be aware of the outcome after set to false
+});
 mongoose.model("Truyen", truyenSchema, "truyen");
-
-
-
-
